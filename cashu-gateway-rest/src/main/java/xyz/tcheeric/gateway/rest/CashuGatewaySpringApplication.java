06e9d0e61ddd9c006d213051d66b5dc4f438cac8
Eric T <tchepane@hey.com>
2025-07-22 02:42:39 +0100

    Use slf4j logging

--- cashu-gateway-rest/src/main/java/xyz/tcheeric/gateway/rest/CashuGatewaySpringApplication.java
+++ cashu-gateway-rest/src/main/java/xyz/tcheeric/gateway/rest/CashuGatewaySpringApplication.java
@@ -3,13 +3,16 @@ package xyz.tcheeric.gateway.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @EntityScan("xyz.tcheeric.gateway.model.entity")
+@Slf4j
 public class CashuGatewaySpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CashuGatewaySpringApplication.class, args);
-	}
+        public static void main(String[] args) {
+                log.info("Starting CashuGatewaySpringApplication");
+                SpringApplication.run(CashuGatewaySpringApplication.class, args);
+        }
 
 }
